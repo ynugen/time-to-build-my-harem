11928926d64ba2a99b91d5cedf76870bd0c13a67
sammi <txl.sammi@gmail.com>
2025-03-08 16:27:07 +1100

    Add recording of movement inputs and playback

--- menu.gd
+++ menu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://player.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level0.tscn")
 	
 
 
