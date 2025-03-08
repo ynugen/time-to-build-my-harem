745fe17175a0c797f334261634104aa093e7b818
sammi <txl.sammi@gmail.com>
2025-03-08 15:17:40 +1100

    Add start button linked to player scene

--- menu.gd
+++ menu.gd
@@ -6,14 +6,8 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-#func _on_start_pressed() -> void:
-#	Add scene in this one
-	#get_tree().change_scene()
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://player.tscn")
 	
 
 
