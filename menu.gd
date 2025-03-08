5c6480144f780caf2e269cf63705ad30b828bd26
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-03-08 14:37:17 +1100

    Create menu scene. Add function to exit button

---
+++ menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+#func _on_start_pressed() -> void:
+#	Add scene in this one
+	#get_tree().change_scene()
+	
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
