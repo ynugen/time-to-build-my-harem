06d88b4e6eca71f212b2b1cf8b038278a6069187
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-03-09 18:14:56 +1100

    Complete start cutscene draft.

--- scenes/cutScenes/cut_scene_0.gd
+++ scenes/cutScenes/cut_scene_0.gd
@@ -10,12 +10,18 @@ func _ready():
 
 # Skips cut scene if space or enter is pressed
 func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
+	if first_time && Input.is_action_pressed("ui_accept"):
 		skip_cutscene()
 
+# Change to level when animation finished
 func _on_animation_finished(animName):
+	first_time = true
 	skip_cutscene()
 
 # Change to level scene
 func skip_cutscene():
 	get_tree().change_scene_to_file("res://scenes/levels/Level0.tscn")
+
+# Skip cutscene if skip button pressed
+func _on_skip_pressed() -> void:
+	skip_cutscene()
