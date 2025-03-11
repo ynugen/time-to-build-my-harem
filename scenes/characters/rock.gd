aeeea51b31ee02f4f1cc5eae53e312e6b852ca2e
sammi <txl.sammi@gmail.com>
2025-03-11 15:42:54 +1100

    yay

--- scenes/characters/rock.gd
+++ scenes/characters/rock.gd
@@ -1,6 +1,10 @@
 extends StaticBody2D
 
 @export var push_distance = 250
+@onready var anim_player = $AnimationPlayer
 
 func push(direction: Vector2):
 	global_position += direction * push_distance
+
+func push_deline():
+	anim_player.play("push_declined")
