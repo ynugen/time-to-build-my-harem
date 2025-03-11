8f4f721c255f98ced23dc0e346b66a48b49b5e8f
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-03-11 15:49:12 +1100

    Update ending scene animations

--- scenes/final_scene.gd
+++ scenes/final_scene.gd
@@ -4,17 +4,27 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	easter_eggs.play("EndingScene")
 
 func _on_play_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/TitleScreen.tscn")
 
 
-func _on_retry_pressed() -> void:
+func _on_exit_pressed() -> void:
+	easter_eggs.play("EndingScene")
+
+
+func _on_lava_button_pressed() -> void:
+	easter_eggs.play("LavaGirl")
+
+
+func _on_leaf_button_pressed() -> void:
 	easter_eggs.play("LeafGirl")
+
+
+func _on_rock_button_pressed() -> void:
+	easter_eggs.play("RockGirl")
+
+
+func _on_slime_button_pressed() -> void:
+	easter_eggs.play("SlimeGirl")
