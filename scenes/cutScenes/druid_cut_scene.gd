f18899f55dd03fd00e6f739268b119c3d398f601
Amy Nguyen <110535408+ynugen@users.noreply.github.com>
2025-03-11 13:05:06 +1100

    Update animations

---
+++ scenes/cutScenes/druid_cut_scene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var animPlayer = $AnimationPlayer
+
+func _ready():
+	animPlayer.play("MeetCute")
+
+
+func _on_success_choice_pressed() -> void:
+	animPlayer.play("Success")
+
+
+func _on_fail_choice_pressed() -> void:
+	animPlayer.play("Fumble")
+
+
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/Level3.tscn")
+
+
+func _on_retry_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/Level2.2.tscn")
